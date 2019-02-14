27df6665ddd8aec2e40da8ef66ac82c73bad17b1
joshcasteel <joshcasteelz@gmail.com>
2019-02-14 17:18:54 -0500

    added more components and fixed routes

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <span className="card-title">Project Title</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur voluptates recusandae, aperiam earum illo vitae aut! Ad possimus distinctio alias a at officiis, saepe aliquam dolore debitis? Aperiam, explicabo delectus?</p>
+            </div>
+            <div className="card-action gret lighten-4 grey-text">
+                <div>Posted by the NN</div>
+                <div>3rd September, 2am</div>
+            </div>
+        </div>
+    )
+}
+export default ProjectDetails;
